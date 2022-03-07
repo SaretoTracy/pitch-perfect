954928beed6d019cfc81e785f286c12e91ba5994
saretotracy <tracysareto@gmail.com>
2022-03-07 19:04:49 +0300

    getting information from form

--- app/auth.py
+++ app/auth.py
@@ -1,15 +1,24 @@
 from flask import Blueprint
-from flask import render_template
+from flask import render_template, request
 from . import auth
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/signup')
+@auth.route('/signup', methods=['GET', 'POST'])
 def signup():
+
+    # getting info from form
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
     return render_template('signup.html')
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+
     return render_template('login.html')
