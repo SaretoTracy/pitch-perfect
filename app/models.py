e75f74b6277ece041514462f1144cf898bf8847d
saretotracy <tracysareto@gmail.com>
2022-03-07 22:57:01 +0300

    connect our class to our database and allow communication

---
+++ app/models.py
@@ -0,0 +1,37 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+
+# a model to define our user data.
+
+
+# connect our class to our database and allow communication.
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    firstname = db.Column(db.String(150))
+    pitches = db.relationship('Pitch', backref='owner')
+    comments = db.relationship('Comment', backref='owner')
+
+    def __repr__(self):
+        return f"User('{self.username}')"
+
+
+class Pitch(db.Model):
+    '''
+    '''
+    __tablename__ = 'pitches'
+
+    id = db.Column(db.Integer, primary_key=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    description = db.Column(db.String(), index=True)
+    title = db.Column(db.String())
+    category = db.Column(db.String(255), nullable=False)
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+
+    def __repr__(self):
+        return f'Pitch {self.description}'
