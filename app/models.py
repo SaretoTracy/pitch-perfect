8e0f076864628314fc9c82cbebdadb81bda48a24
saretotracy <tracysareto@gmail.com>
2022-03-08 11:25:15 +0300

    Securing our passwords

--- app/models.py
+++ app/models.py
@@ -1,7 +1,11 @@
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
+from werkzeug.security import generate_password_hash, check_password_hash
 
+
+# generating_password_hash - This function takes in a password and generates a password hash.
+# check_password_hash - This function takes in a hash password and a password entered by a user and checks if the password matches to return a True or False
 
 # a model to define our user data.
 
@@ -32,6 +36,18 @@ class Pitch(db.Model):
     title = db.Column(db.String())
     category = db.Column(db.String(255), nullable=False)
     date = db.Column(db.DateTime(timezone=True), default=func.now())
+    pass_secure = db.Column(db.String(255))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.pass_secure, password)
 
     def __repr__(self):
         return f'Pitch {self.description}'
