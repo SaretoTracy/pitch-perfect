c172b7908c8224716b51578667797632ede9aa2c
saretotracy <tracysareto@gmail.com>
2022-03-11 10:43:46 +0300

    add upvote downvote functionality

--- app/auth.py
+++ app/auth.py
@@ -85,20 +85,3 @@ def dashboard():
         db.session.add(comment)
         db.session.commit()
     return render_template('dashboard.html', name=current_user.username, pitch=pitch, form=form, content=form.content.data)
-
-
-# @auth.route('/register', methods=["GET", "POST"])
-# def register():
-#     form = RegistrationForm()
-#     if form.validate_on_submit():
-#         user = User(email=form.email.data,
-#                     username=form.username.data, password=form.password.data)
-#         db.session.add(user)
-#         db.session.commit()
-
-#         mail_message("Welcome to watchlist",
-#                      "email/welcome_user", user.email, user=user)
-
-#         return redirect(url_for('auth.login'))
-#         title = "New Account"
-#     return render_template('auth/register.html', registration_form=form)
